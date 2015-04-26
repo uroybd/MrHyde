fd082aa52416b7304ab0415a547d200fe485be4f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-04-26 23:47:27 +0200

    client: debug menu option to delete all files

--- client/app/src/main/java/org/faudroids/mrhyde/ui/MainDrawerActivity.java
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/MainDrawerActivity.java
@@ -1,13 +1,21 @@
 package org.faudroids.mrhyde.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import org.faudroids.mrhyde.R;
 import org.faudroids.mrhyde.github.LoginManager;
+
+import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Inject;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 import it.neokree.materialnavigationdrawer.elements.MaterialAccount;
+import timber.log.Timber;
 
 
 public class MainDrawerActivity extends AbstractRoboDrawerActivity {
@@ -26,6 +34,40 @@ public class MainDrawerActivity extends AbstractRoboDrawerActivity {
                 account.getAvatar(),
                 null));
         setBackPattern(MaterialNavigationDrawer.BACKPATTERN_BACK_TO_FIRST);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.clear:
+                try {
+                    delete(getFilesDir());
+                } catch (IOException e) {
+                    Timber.e(e, "failed to delete files");
+                    throw new RuntimeException(e);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void delete(File file) throws IOException {
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                delete(f);
+            }
+        }
+        if (!file.equals(getFilesDir())) file.delete();
     }
 
 }
