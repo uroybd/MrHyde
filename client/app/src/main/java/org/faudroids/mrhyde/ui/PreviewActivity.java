bac3927cc762f8fda058282af162a48c5bbe9f87
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-05-02 16:19:16 +0200

    client: readd webview for preview
    Seems to work after all ...

--- client/app/src/main/java/org/faudroids/mrhyde/ui/PreviewActivity.java
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/PreviewActivity.java
@@ -1,10 +1,13 @@
 package org.faudroids.mrhyde.ui;
 
 import android.annotation.TargetApi;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebBackForwardList;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.eclipse.egit.github.core.Repository;
 import org.faudroids.mrhyde.R;
@@ -16,19 +19,23 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 import rx.functions.Action1;
 import timber.log.Timber;
 
 @ContentView(R.layout.activity_preview)
 public class PreviewActivity extends AbstractActionBarActivity {
 
-    private static final int REQUEST_BROWSER = 44;
+    private static final String STATE_URL = "STATE_URL";
 
     static final String
             EXTRA_REPO = "EXTRA_REPO",
             EXTRA_DIFF = "EXTRA_DIFF";
 
     @Inject PreviewManager previewManager;
+    @InjectView(R.id.web_view) WebView webView;
+
+    private String previewUrl;
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -39,37 +46,63 @@ public class PreviewActivity extends AbstractActionBarActivity {
         Repository repository = (Repository) getIntent().getSerializableExtra(EXTRA_REPO);
         String diff = getIntent().getStringExtra(EXTRA_DIFF);
 
-		showSpinner();
-		compositeSubscription.add(previewManager
-				.loadPreview(repository, diff)
-				.delay(0, TimeUnit.SECONDS)
-				.compose(new DefaultTransformer<String>())
-				.subscribe(new Action1<String>() {
-					@Override
-					public void call(String previewUrl) {
-						hideSpinner();
-						Timber.d("getting url " + previewUrl);
-						startBrowser(previewUrl);
-					}
-				}, new Action1<Throwable>() {
-					@Override
-					public void call(Throwable throwable) {
-						hideSpinner();
-						Timber.e(throwable, "failed to get results from server");
-					}
-				}));
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage message) {
+                Timber.d(message.message());
+                return true;
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return false;
+            }
+        });
+
+        // load preview
+        if (savedInstanceState != null) {
+            previewUrl = savedInstanceState.getString(STATE_URL);
+            webView.restoreState(savedInstanceState);
+
+        } else {
+            showSpinner();
+            compositeSubscription.add(previewManager
+                    .loadPreview(repository, diff)
+                    .delay(0, TimeUnit.SECONDS)
+                    .compose(new DefaultTransformer<String>())
+                    .subscribe(new Action1<String>() {
+                        @Override
+                        public void call(String previewUrl) {
+                            hideSpinner();
+                            Timber.d("getting url " + previewUrl);
+                            webView.loadUrl(previewUrl);
+                            // startBrowser(previewUrl);
+                        }
+                    }, new Action1<Throwable>() {
+                        @Override
+                        public void call(Throwable throwable) {
+                            hideSpinner();
+                            Timber.e(throwable, "failed to get results from server");
+                        }
+                    }));
+        }
     }
 
 
     @Override
-    public void onActivityResult(int requestCode, int result, Intent data) {
-        finish();
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable(STATE_URL, previewUrl);
+        webView.saveState(outState);
     }
 
 
-    private void startBrowser(String previewUrl) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(previewUrl));
-        startActivityForResult(browserIntent, REQUEST_BROWSER);
+    @Override
+    public void onBackPressed() {
+        WebBackForwardList backForwardList = webView.copyBackForwardList();
+        if (backForwardList.getCurrentIndex() > 1) webView.goBack();
+        else super.onBackPressed();
     }
 
 }
