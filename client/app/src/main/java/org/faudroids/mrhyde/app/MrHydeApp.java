bbb790f378a13bb817076241f90242f0bf873300
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-05-03 00:20:37 +0200

    client: setup crashlytics

--- client/app/src/main/java/org/faudroids/mrhyde/app/MrHydeApp.java
+++ client/app/src/main/java/org/faudroids/mrhyde/app/MrHydeApp.java
@@ -3,10 +3,13 @@ package org.faudroids.mrhyde.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 import org.faudroids.mrhyde.BuildConfig;
 import org.faudroids.mrhyde.github.GitHubModule;
 import org.faudroids.mrhyde.jekyll.JekyllModule;
 
+import io.fabric.sdk.android.Fabric;
 import roboguice.RoboGuice;
 import timber.log.Timber;
 
@@ -23,10 +26,39 @@ public final class MrHydeApp extends Application {
 				new GitHubModule(),
 				new JekyllModule());
 
-		if (BuildConfig.DEBUG) {
+		if (!BuildConfig.DEBUG) {
 			Timber.plant(new Timber.DebugTree());
 		} else {
-			throw new UnsupportedOperationException("setup logging and crashlytics");
+			Fabric.with(this, new Crashlytics());
+			Timber.plant(new CrashReportingTree());
 		}
+	}
+
+
+	private static final class CrashReportingTree extends Timber.HollowTree {
+
+		@Override
+		public void e(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void e(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
+
+		@Override
+		public void w(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void w(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
 	}
 }
