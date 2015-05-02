b0220af82b0ab0c4c3954a53b8fcaf53efd75928
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-05-02 12:49:50 +0200

    client: start browser for preview
    The webview simple is not reliable enough

--- client/app/src/main/java/org/faudroids/mrhyde/ui/PreviewActivity.java
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/PreviewActivity.java
@@ -1,13 +1,10 @@
 package org.faudroids.mrhyde.ui;
 
 import android.annotation.TargetApi;
-import android.net.http.SslError;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import org.eclipse.egit.github.core.Repository;
 import org.faudroids.mrhyde.R;
@@ -19,80 +16,60 @@ import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 
 import roboguice.inject.ContentView;
-import roboguice.inject.InjectView;
 import rx.functions.Action1;
 import timber.log.Timber;
 
 @ContentView(R.layout.activity_preview)
 public class PreviewActivity extends AbstractActionBarActivity {
 
-    private static final String STATE_URL = "STATE_URL";
+    private static final int REQUEST_BROWSER = 44;
 
     static final String
             EXTRA_REPO = "EXTRA_REPO",
             EXTRA_DIFF = "EXTRA_DIFF";
 
-    @InjectView(R.id.web_view) WebView webView;
     @Inject PreviewManager previewManager;
-
-    private String previewUrl;
-
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // enable javascript + ignore SSL errors
-        webView.getSettings().setJavaScriptEnabled(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-        });
-
         // load arguments
         Repository repository = (Repository) getIntent().getSerializableExtra(EXTRA_REPO);
         String diff = getIntent().getStringExtra(EXTRA_DIFF);
 
-        // load preview
-        if (savedInstanceState != null) {
-            this.previewUrl = savedInstanceState.getString(STATE_URL, null);
-            Timber.d("restoring url " + previewUrl);
-            webView.loadUrl(previewUrl);
-        } else {
-            showSpinner();
-            compositeSubscription.add(previewManager
-                    .loadPreview(repository, diff)
-                    .delay(1, TimeUnit.SECONDS)
-                    .compose(new DefaultTransformer<String>())
-                    .subscribe(new Action1<String>() {
-                        @Override
-                        public void call(String previewUrl) {
-                            hideSpinner();
-                            Timber.d("getting url " + previewUrl);
-                            webView.loadUrl(previewUrl);
-                            PreviewActivity.this.previewUrl = previewUrl;
-                        }
-                    }, new Action1<Throwable>() {
-                        @Override
-                        public void call(Throwable throwable) {
-                            hideSpinner();
-                            Timber.e(throwable, "failed to get results from server");
-                        }
-                    }));
-        }
+		showSpinner();
+		compositeSubscription.add(previewManager
+				.loadPreview(repository, diff)
+				.delay(0, TimeUnit.SECONDS)
+				.compose(new DefaultTransformer<String>())
+				.subscribe(new Action1<String>() {
+					@Override
+					public void call(String previewUrl) {
+						hideSpinner();
+						Timber.d("getting url " + previewUrl);
+						startBrowser(previewUrl);
+					}
+				}, new Action1<Throwable>() {
+					@Override
+					public void call(Throwable throwable) {
+						hideSpinner();
+						Timber.e(throwable, "failed to get results from server");
+					}
+				}));
     }
 
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(STATE_URL, previewUrl);
+    public void onActivityResult(int requestCode, int result, Intent data) {
+        finish();
+    }
+
+
+    private void startBrowser(String previewUrl) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(previewUrl));
+        startActivityForResult(browserIntent, REQUEST_BROWSER);
     }
 
 }
