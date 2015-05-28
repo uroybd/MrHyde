2598fbd0cd087491cca9b34d5a7d12d8ca45be8c
Simon Hofmann <mail@simon-hofmann.org>
2015-05-28 08:35:25 +0200

    server: Added CSS to expired repo pages

--- server/caretaker.py
+++ server/caretaker.py
@@ -22,11 +22,19 @@ class Caretaker:
     <head>
     <meta charset="utf-8">
     <title>MrHyde - a Jekyll scratchpad</title>
+    <link rel="stylesheet" href="static/layout.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
     </head>
 
-    <body bgcolor="white" text="black">
-        <p><strong>Link expired!</strong></p>
-        <p>Preview links expire after %s minutes.</p>
+    <body>
+        <div style="text-align: center;">
+            <p><strong>Link expired!</strong></p>
+            <p>Preview links expire after %s minutes.</p>
+        </div>
+        <p></p>
+        <div align="center"><img src="static/ic_background.svg" alt="MrHyde logo"></div>
+        <p></p>
+        <footer style="text-align: center;">&copy; FauDroids 2015</footer>
     </body>
 </html>
 """
