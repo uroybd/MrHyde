b06411e23a3ced9df8acbaa601860d086f5965a5
Simon Hofmann <mail@simon-hofmann.org>
2015-06-01 16:06:33 +0200

    webpage: Deleted and moved files to work with gh-pages

--- client/README.md
+++
@@ -1,2 +0,0 @@
-# MrHyde
-Avoid turning yourself into a raging monster by using our Android app to ease blogging using Jekyll and GitHub pages.
--- server/setup.sql
+++
@@ -1,1 +0,0 @@
-create table repo(id text not null, path text not null, deploy_path text not null, url text not null, last_used int not null, active int not null);
