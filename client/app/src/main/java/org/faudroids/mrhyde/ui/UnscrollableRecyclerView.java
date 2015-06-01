29b1d64cad8405c2dd6acd2b34eb1b8ca4330281
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-06-01 23:28:02 +0200

    ui: fix scrollTo crash in recycle view

---
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/UnscrollableRecyclerView.java
@@ -0,0 +1,34 @@
+package org.faudroids.mrhyde.ui;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+import timber.log.Timber;
+
+/**
+ * On some devices the scollTo method is called with leads to an UnsupportedOperationException.
+ * This class tries to fix that.
+ * Courtesy to http://stackoverflow.com/a/28432632
+ */
+public class UnscrollableRecyclerView extends RecyclerView {
+
+	public UnscrollableRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	public UnscrollableRecyclerView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public UnscrollableRecyclerView(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void scrollTo(int x, int y) {
+		Timber.w("scrolling not supported");
+	}
+
+}
