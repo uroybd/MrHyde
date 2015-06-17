b5eb15c60786084380dc1f3dbf54b485671fc501
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-06-18 01:26:25 +0200

    client: add AbstractJekyllActivity

---
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/AbstractJekyllActivity.java
@@ -0,0 +1,172 @@
+package org.faudroids.mrhyde.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.getbase.floatingactionbutton.AddFloatingActionButton;
+
+import org.eclipse.egit.github.core.Repository;
+import org.faudroids.mrhyde.R;
+import org.faudroids.mrhyde.jekyll.JekyllManager;
+import org.faudroids.mrhyde.jekyll.JekyllManagerFactory;
+import org.faudroids.mrhyde.ui.utils.AbstractActionBarActivity;
+import org.faudroids.mrhyde.ui.utils.DividerItemDecoration;
+import org.faudroids.mrhyde.ui.utils.JekyllUiUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
+
+@ContentView(R.layout.activity_posts_or_drafts)
+abstract class AbstractJekyllActivity<T> extends AbstractActionBarActivity {
+
+	private static final int REQUEST_COMMIT = 42;
+
+	static final String EXTRA_REPOSITORY = "EXTRA_REPOSITORY";
+
+
+	@InjectView(R.id.list) private RecyclerView recyclerView;
+	protected AbstractAdapter adapter;
+	@Inject JekyllUiUtils jekyllUiUtils;
+
+	@InjectView(R.id.add) AddFloatingActionButton addButton;
+
+	protected Repository repository;
+	@Inject private JekyllManagerFactory jekyllManagerFactory;
+	protected JekyllManager jekyllManager;
+
+	@Inject private ActivityIntentFactory intentFactory;
+
+	private final int titleStringResource;
+
+	AbstractJekyllActivity(int titleStringResource) {
+		this.titleStringResource = titleStringResource;
+	}
+
+
+	protected abstract void onAddClicked();
+	protected abstract void loadItems();
+	protected abstract AbstractAdapter createAdapter();
+
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// get arguments
+		repository = (Repository) getIntent().getSerializableExtra(EXTRA_REPOSITORY);
+		jekyllManager = jekyllManagerFactory.createJekyllManager(repository);
+
+		// set title
+		setTitle(getString(titleStringResource));
+
+		// setup list
+		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+		adapter = createAdapter();
+		recyclerView.setLayoutManager(layoutManager);
+		recyclerView.setAdapter(adapter);
+		recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+
+		// setup add
+		addButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onAddClicked();
+			}
+		});
+
+		// load posts
+		loadItems();
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.posts, menu);
+		return true;
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_commit:
+				startActivityForResult(intentFactory.createCommitIntent(repository), REQUEST_COMMIT);
+				return true;
+
+			case R.id.action_preview:
+				startActivity(intentFactory.createPreviewIntent(repository));
+				return true;
+
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+	@Override
+	public void onActivityResult(int request, int result, Intent data) {
+		switch (request) {
+			case REQUEST_COMMIT:
+				if (result != RESULT_OK) return;
+				showSpinner();
+				loadItems();
+		}
+	}
+
+
+	abstract class AbstractAdapter extends RecyclerView.Adapter<AbstractAdapter.AbstractViewHolder> {
+
+		private final List<T> itemsList = new ArrayList<>();
+
+		@Override
+		public AbstractViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_overview_post, parent, false);
+			return doOnCreateViewHolder(view);
+		}
+
+		protected abstract AbstractViewHolder doOnCreateViewHolder(View view);
+
+		@Override
+		public void onBindViewHolder(AbstractViewHolder holder, int position) {
+			holder.setItem(itemsList.get(position));
+		}
+
+		@Override
+		public int getItemCount() {
+			return itemsList.size();
+		}
+
+		public void setItems(List<T> itemsList) {
+			this.itemsList.clear();
+			this.itemsList.addAll(itemsList);
+			notifyDataSetChanged();
+		}
+
+
+		abstract class AbstractViewHolder extends RecyclerView.ViewHolder {
+
+			protected final View view;
+
+			public AbstractViewHolder(View view) {
+				super(view);
+				this.view = view;
+			}
+
+			protected abstract void setItem(T item);
+		}
+	}
+
+}
--- client/app/src/main/java/org/faudroids/mrhyde/ui/PostsActivity.java
+++ client/app/src/main/java/org/faudroids/mrhyde/ui/PostsActivity.java
@@ -1,100 +1,39 @@
 package org.faudroids.mrhyde.ui;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-import com.getbase.floatingactionbutton.AddFloatingActionButton;
-
-import org.eclipse.egit.github.core.Repository;
 import org.faudroids.mrhyde.R;
-import org.faudroids.mrhyde.jekyll.JekyllManager;
-import org.faudroids.mrhyde.jekyll.JekyllManagerFactory;
 import org.faudroids.mrhyde.jekyll.Post;
-import org.faudroids.mrhyde.ui.utils.AbstractActionBarActivity;
-import org.faudroids.mrhyde.ui.utils.DividerItemDecoration;
-import org.faudroids.mrhyde.ui.utils.JekyllUiUtils;
 import org.faudroids.mrhyde.utils.DefaultErrorAction;
 import org.faudroids.mrhyde.utils.DefaultTransformer;
 import org.faudroids.mrhyde.utils.ErrorActionBuilder;
 import org.faudroids.mrhyde.utils.HideSpinnerAction;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
-import roboguice.inject.ContentView;
-import roboguice.inject.InjectView;
 import rx.functions.Action1;
 
-@ContentView(R.layout.activity_posts_or_drafts)
-public final class PostsActivity extends AbstractActionBarActivity {
+public class PostsActivity extends AbstractJekyllActivity<Post> {
 
-	private static final int REQUEST_COMMIT = 42;
-
-	static final String EXTRA_REPOSITORY = "EXTRA_REPOSITORY";
-
-
-	@InjectView(R.id.list) private RecyclerView recyclerView;
-	private PostsAdapter postsAdapter;
-	@Inject JekyllUiUtils jekyllUiUtils;
-
-	@InjectView(R.id.add) AddFloatingActionButton addButton;
-
-	private Repository repository;
-	@Inject private JekyllManagerFactory jekyllManagerFactory;
-	private JekyllManager jekyllManager;
-
-	@Inject private ActivityIntentFactory intentFactory;
-
-
-	@Override
-	public void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		// get arguments
-		repository = (Repository) getIntent().getSerializableExtra(EXTRA_REPOSITORY);
-		jekyllManager = jekyllManagerFactory.createJekyllManager(repository);
-
-		// set title
-		setTitle(getString(R.string.posts));
-
-		// setup list
-		RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-		postsAdapter = new PostsAdapter();
-		recyclerView.setLayoutManager(layoutManager);
-		recyclerView.setAdapter(postsAdapter);
-		recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-
-		// setpu add
-		addButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				jekyllUiUtils.showNewPostDialog(jekyllManager, repository);
-			}
-		});
-
-		// load posts
-		loadPosts();
+	public PostsActivity() {
+		super(R.string.posts);
 	}
 
 
-	private void loadPosts() {
+	@Override
+	protected void onAddClicked() {
+		jekyllUiUtils.showNewPostDialog(jekyllManager, repository);
+	}
+
+	@Override
+	protected void loadItems() {
 		compositeSubscription.add(jekyllManager.getAllPosts()
 				.compose(new DefaultTransformer<List<Post>>())
 				.subscribe(new Action1<List<Post>>() {
 					@Override
 					public void call(List<Post> posts) {
 						if (isSpinnerVisible()) hideSpinner();
-						postsAdapter.setPosts(posts);
+						adapter.setItems(posts);
 					}
 				}, new ErrorActionBuilder()
 						.add(new DefaultErrorAction(PostsActivity.this, "failed to load posts"))
@@ -102,82 +41,30 @@ public final class PostsActivity extends AbstractActionBarActivity {
 						.build()));
 	}
 
-
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.posts, menu);
-		return true;
+	protected AbstractAdapter createAdapter() {
+		return new PostsAdapter();
 	}
 
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO
-		switch (item.getItemId()) {
-			case R.id.action_commit:
-				startActivityForResult(intentFactory.createCommitIntent(repository), REQUEST_COMMIT);
-				return true;
-
-			case R.id.action_preview:
-				startActivity(intentFactory.createPreviewIntent(repository));
-				return true;
-
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-
-	@Override
-	public void onActivityResult(int request, int result, Intent data) {
-		switch (request) {
-			case REQUEST_COMMIT:
-				if (result != RESULT_OK) return;
-				showSpinner();
-				loadPosts();
-		}
-	}
-
-
-	public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostViewHolder> {
-
-		private final List<Post> postsList = new ArrayList<>();
+	public class PostsAdapter extends AbstractAdapter {
 
 		@Override
-		public PostViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_overview_post, parent, false);
+		protected AbstractViewHolder doOnCreateViewHolder(View view) {
 			return new PostViewHolder(view);
 		}
 
-		@Override
-		public void onBindViewHolder(PostViewHolder holder, int position) {
-			holder.setPost(postsList.get(position));
-		}
-
-		@Override
-		public int getItemCount() {
-			return postsList.size();
-		}
-
-		public void setPosts(List<Post> postsList) {
-			this.postsList.clear();
-			this.postsList.addAll(postsList);
-			notifyDataSetChanged();
-		}
-
-
-		public class PostViewHolder extends RecyclerView.ViewHolder {
-
-			private final View view;
+		public class PostViewHolder extends AbstractViewHolder {
 
 			public PostViewHolder(View view) {
 				super(view);
-				this.view = view;
 			}
 
-			public void setPost(Post post) {
-				jekyllUiUtils.setPostOverview(view, post, repository);
+			@Override
+			protected void setItem(Post item) {
+				jekyllUiUtils.setPostOverview(view, item, repository);
 			}
+
 		}
 	}
 
